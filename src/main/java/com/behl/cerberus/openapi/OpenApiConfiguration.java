87f2be4a949656e7dd1028089d0eec04c5cd6e97
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-06-26 14:43:43 +0530

    adding open-api configuration

---
+++ src/main/java/com/behl/cerberus/openapi/OpenApiConfiguration.java
@@ -0,0 +1,38 @@
+package com.behl.cerberus.openapi;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.behl.cerberus.openapi.properties.OpenApiConfigurationProperties;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableConfigurationProperties(OpenApiConfigurationProperties.class)
+@AllArgsConstructor
+public class OpenApiConfiguration {
+
+	private final OpenApiConfigurationProperties openApiConfigurationProperties;
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		final var properties = openApiConfigurationProperties.getOpenApi();
+		final var security = properties.getSecurity();
+		final var contact = properties.getContact();
+		final var info = new Info().title(properties.getTitle()).version(properties.getApiVersion())
+				.description(properties.getDescription())
+				.contact(new Contact().email(contact.getEmail()).name(contact.getName()).url(contact.getUrl()));
+
+		return new OpenAPI().info(info).addSecurityItem(new SecurityRequirement().addList(security.getName()))
+				.components(new Components().addSecuritySchemes(security.getName(),
+						new SecurityScheme().name(security.getName()).type(SecurityScheme.Type.HTTP)
+								.scheme(security.getScheme()).bearerFormat(security.getBearerFormat())));
+	}
+}
---
+++ src/main/java/com/behl/cerberus/openapi/properties/OpenApiConfigurationProperties.java
@@ -0,0 +1,36 @@
+package com.behl.cerberus.openapi.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties(prefix = "com.behl.cerberus")
+public class OpenApiConfigurationProperties {
+
+	private OpenAPI openApi = new OpenAPI();
+
+	@Data
+	public class OpenAPI {
+		private String title;
+		private String description;
+		private String apiVersion;
+		private Contact contact = new Contact();
+		private Security security = new Security();
+
+		@Data
+		public class Contact {
+			private String email;
+			private String name;
+			private String url;
+		}
+
+		@Data
+		public class Security {
+			private String name;
+			private String scheme;
+			private String bearerFormat;
+		}
+	}
+
+}
