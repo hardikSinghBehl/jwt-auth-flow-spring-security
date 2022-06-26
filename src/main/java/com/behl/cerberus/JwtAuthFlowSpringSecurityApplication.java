758699a896d61d0ae067c4dabaef328be7f6f607
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-06-26 14:27:51 +0530

    project initialization

---
+++ src/main/java/com/behl/cerberus/JwtAuthFlowSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.behl.cerberus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthFlowSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthFlowSpringSecurityApplication.class, args);
+	}
+
+}
