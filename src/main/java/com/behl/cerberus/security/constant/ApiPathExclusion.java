07597249ec42fb60e0a33a3d5d57abd385f9d409
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-09-26 23:14:40 +0530

    refactor: deleting health check endpoint

--- src/main/java/com/behl/cerberus/security/constant/ApiPathExclusion.java
+++ src/main/java/com/behl/cerberus/security/constant/ApiPathExclusion.java
@@ -11,7 +11,7 @@ public class ApiPathExclusion {
 	@AllArgsConstructor
 	public enum GetApiPathExclusion {
 
-		HEALTH_CHECK("/ping"), SWAGGER_API_V2_DOCS("/v2/api-docs"),
+		SWAGGER_API_V2_DOCS("/v2/api-docs"),
 		SWAGGER_RESOURCE_CONFIGURATION("/swagger-resources/configuration/ui"), SWAGGER_RESOURCES("/swagger-resources"),
 		SWAGGER_RESOURCES_SECURITY_CONFIGURATION("/swagger-resources/configuration/security"),
 		SWAGGER_UI_HTML("swagger-ui.html"), WEBJARS("/webjars/**"), SWAGGER_UI("/swagger-ui/**"),
