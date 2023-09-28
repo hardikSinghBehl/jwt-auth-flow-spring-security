29919bf34eda6ff0637f0635ae3ff70dd1cd3ba5
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-09-28 21:48:18 +0530

    consolidating security beans

--- src/main/java/com/behl/cerberus/configuration/SecurityConfiguration.java
+++ src/main/java/com/behl/cerberus/configuration/SecurityConfiguration.java
@@ -7,8 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 @EnableConfigurationProperties(ApiPathExclusionConfigurationProperties.class)
 public class SecurityConfiguration {
@@ -52,6 +52,11 @@ public class SecurityConfiguration {
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
