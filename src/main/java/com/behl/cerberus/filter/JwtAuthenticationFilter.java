7ff1c7c560328a7bd53b845c72988f7cd509ac34
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-09-28 23:25:35 +0530

    skipping jwt filter execution for unsecured API invocations

--- src/main/java/com/behl/cerberus/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/behl/cerberus/filter/JwtAuthenticationFilter.java
@@ -1,17 +1,24 @@
 package com.behl.cerberus.filter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.behl.cerberus.configuration.ApiPathExclusionConfigurationProperties;
 import com.behl.cerberus.repository.UserRepository;
 import com.behl.cerberus.utility.JwtUtility;
 
+import io.swagger.v3.oas.models.PathItem.HttpMethod;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,10 +27,12 @@ import lombok.SneakyThrows;
 
 @Component
 @RequiredArgsConstructor
+@EnableConfigurationProperties(ApiPathExclusionConfigurationProperties.class)
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtUtility jwtUtils;
 	private final UserRepository userRepository;
+	private final ApiPathExclusionConfigurationProperties apiPathExclusionConfigurationProperties;
 	
 	private static final String AUTHORIZATION_HEADER = "Authorization";
 	private static final String BEARER_PREFIX = "Bearer ";
@@ -31,24 +40,49 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	@SneakyThrows
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
-		final var authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
-
-		if (StringUtils.isNotEmpty(authorizationHeader)) {
-			if (authorizationHeader.startsWith(BEARER_PREFIX)) {
-				final var token = authorizationHeader.replace(BEARER_PREFIX, StringUtils.EMPTY);
-				final var userId = jwtUtils.extractUserId(token);
-				final var isTokenValid = jwtUtils.validateToken(token, userId);
-				
-				if (Boolean.TRUE.equals(isTokenValid)) {
-					final var user = userRepository.findById(userId).orElseThrow(IllegalStateException::new);
-					final var authentication = new UsernamePasswordAuthenticationToken(
-							user.getEmailId(), user.getPassword(), List.of());
-					authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-					SecurityContextHolder.getContext().setAuthentication(authentication);
+		final var unsecuredApiBeingInvoked = isInvocationToUnsecuredApi(request);
+		
+		if (Boolean.FALSE.equals(unsecuredApiBeingInvoked)) {
+			final var authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+	
+			if (StringUtils.isNotEmpty(authorizationHeader)) {
+				if (authorizationHeader.startsWith(BEARER_PREFIX)) {
+					final var token = authorizationHeader.replace(BEARER_PREFIX, StringUtils.EMPTY);
+					final var userId = jwtUtils.extractUserId(token);
+					final var isTokenValid = jwtUtils.validateToken(token, userId);
+					
+					if (Boolean.TRUE.equals(isTokenValid)) {
+						final var user = userRepository.findById(userId).orElseThrow(IllegalStateException::new);
+						final var authentication = new UsernamePasswordAuthenticationToken(
+								user.getEmailId(), user.getPassword(), List.of());
+						authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+						SecurityContextHolder.getContext().setAuthentication(authentication);
+					}
 				}
 			}
 		}
 		filterChain.doFilter(request, response);
+	}
+	
+	private boolean isInvocationToUnsecuredApi(final HttpServletRequest request) {
+	    final var requestHttpMethod = HttpMethod.valueOf(request.getMethod());
+	    var unsecuredApiPaths = getUnsecuredApiPaths(requestHttpMethod);
+	    unsecuredApiPaths = Optional.ofNullable(unsecuredApiPaths).orElseGet(ArrayList::new);
+
+	    return unsecuredApiPaths.stream().anyMatch(apiPath -> new AntPathMatcher().match(apiPath, request.getRequestURI()));
+	}
+	
+	private List<String> getUnsecuredApiPaths(final HttpMethod httpMethod) {
+	    switch (httpMethod) {
+	        case GET:
+	            return apiPathExclusionConfigurationProperties.getGet();
+	        case POST:
+	            return apiPathExclusionConfigurationProperties.getPost();
+	        case PUT:
+	            return apiPathExclusionConfigurationProperties.getPut();
+	        default:
+	            return Collections.emptyList();
+	    }
 	}
 
 }
