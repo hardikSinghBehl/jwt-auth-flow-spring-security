8c82a23b31caa6f8fc3aa2dd47560bb09838c45d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-10-01 23:44:01 +0530

    adding scopes in JWT and security context

--- src/main/java/com/behl/cerberus/utility/JwtUtility.java
+++ src/main/java/com/behl/cerberus/utility/JwtUtility.java
@@ -2,21 +2,28 @@ package com.behl.cerberus.utility;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import com.behl.cerberus.configuration.JwtConfigurationProperties;
+import com.behl.cerberus.entity.User;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +35,7 @@ public class JwtUtility {
 	private final JwtConfigurationProperties jwtConfigurationProperties;
 	
 	private static final String BEARER_PREFIX = "Bearer ";
+	private static final String SCOPE_CLAIM_NAME = "scp";
 
 	@Value("${spring.application.name}")
 	private String issuer;
@@ -37,19 +45,31 @@ public class JwtUtility {
 		return UUID.fromString(audience);
 	}
 	
-	public String generateAccessToken(final UUID userId) {
+	public String generateAccessToken(final User user) {
 		final var accessTokenValidity = jwtConfigurationProperties.getJwt().getAccessToken().getValidity();
 		final var expiration = TimeUnit.MINUTES.toMillis(accessTokenValidity);
 		final var secretKey = jwtConfigurationProperties.getJwt().getSecretKey();
 		final var currentTimestamp = new Date(System.currentTimeMillis());
 		final var expirationTimestamp = new Date(System.currentTimeMillis() + expiration);
+		final var scopes = user.getUserStatus().getScopes().stream().collect(Collectors.joining(StringUtils.SPACE));
+		
+		final var claims = new DefaultClaims();
+		claims.put(SCOPE_CLAIM_NAME, scopes);
 		
 		return Jwts.builder()
+				.setClaims(claims)
 				.setIssuer(issuer)
 				.setIssuedAt(currentTimestamp)
 				.setExpiration(expirationTimestamp)
-				.setAudience(String.valueOf(userId))
+				.setAudience(String.valueOf(user.getId()))
 				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+	}
+	
+	public List<GrantedAuthority> getAuthority(@NonNull final String token){
+		final var scopes = extractClaim(token, claims -> claims.get(SCOPE_CLAIM_NAME, String.class));
+		return Arrays.stream(scopes.split(StringUtils.SPACE))
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
 	}
 
 	public Boolean validateToken(@NonNull final String token, @NonNull final UUID userId) {
