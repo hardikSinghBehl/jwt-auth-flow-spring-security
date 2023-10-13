936cdf0907e987f790488aaf570879db2687870d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2023-10-13 11:41:39 +0530

    handling empty refresh token header case

--- src/main/java/com/behl/cerberus/utility/RefreshTokenHeaderProvider.java
+++ src/main/java/com/behl/cerberus/utility/RefreshTokenHeaderProvider.java
@@ -2,6 +2,7 @@ package com.behl.cerberus.utility;
 
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,7 +17,8 @@ public class RefreshTokenHeaderProvider {
 	public static final String REFRESH_TOKEN_HEADER = "X-Refresh-Token";
 	
 	public Optional<String> getRefreshToken() {
-		return Optional.ofNullable(httpServletRequest.getHeader(REFRESH_TOKEN_HEADER));
+		return Optional.ofNullable(httpServletRequest.getHeader(REFRESH_TOKEN_HEADER))
+				.filter(value -> StringUtils.isNotBlank(value));
 	}
 
 }
